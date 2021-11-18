dc58f6ea9008661854b61c11f47ee8c30524f49e
Seanerlars <Sean.larsen@bryanuniversity.edu>
2021-11-18 14:43:06 -0600

    first commit

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+
+export default function App() {
+  export default function App () {
+    return (
+      <main className="text-gray-400 bg-gray-900 body-font">
+        <navbar>
+          <About />
+          <Projects />
+          <Skills />
+          <Contact />
+        </navbar>
+      </main>
+    );
+  }
+
+  
+  return "App";
+}
